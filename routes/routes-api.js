c8b482b4ca1531a9c4f28d43702307807f4d8894
Rael Wanjala <raelnwanjala@gmail.com>
2023-09-06 03:11:24 -0700

    Created a Get and Post request to api/notes. On server.js I set up express.js to serve the api and html routes.

--- routes/routes-api.js
+++ routes/routes-api.js
@@ -2,3 +2,24 @@
 const router = require("express").Router();
 const { v4: uuidv4 } = require("uuid");
 const fs = require("fs");
+
+//Define the POST request to /api/notes and return contents of the JSON file on db/db.json
+router.get("/api/notes", async (req, res) => {
+  const dbJson = await JSON.parse(fs.readFileSync("db/db.json", "utf8"));
+  res.json(dbJson);
+});
+
+//Define the POST request to /api/notes
+router.post("/api/notes", (req, res) => {
+  const dbJson = JSON.parse(fs.readFileSync("db/db.json", "utf8"));
+  const newFeedback = {
+    title: req.body.title,
+    text: req.body.text,
+    id: uuidv4(),
+  };
+  dbJson.push(newFeedback);
+  fs.writeFileSync("db/db.json", JSON.stringify(dbJson));
+  res.json(dbJson);
+});
+
+module.exports = router;
